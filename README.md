3840327be42d3a9154566912319ff2a74ad5b10c
Asia Zioła <ziola.jm@gmail.com>
2020-10-18 17:33:37 +0200

    immutability & additional buttons

---
+++ README.md
@@ -0,0 +1,15 @@
+# Simple task list
+
+### Demo
+
+https://asiaziola.github.io/to-do-list/
+
+### Technologies used
+
+- HTML
+- CSS
+- JS ES6
+
+### Methodologies
+
+- BEM
--- js/to-do-list.js
+++ js/to-do-list.js
@@ -1,6 +1,8 @@
 {
-    const tasks = [
+    let tasks = [
     ];
+
+    let hideDoneTasks = false;
 
     const bindEvents = () => {
         const removeButtons = document.querySelectorAll(".js-removeButton");
@@ -20,40 +22,88 @@
         });
     };
 
-    const render = () => {
+    const bindAllEvent = () => {
+        if (tasks.length > 0) {
+            const doneAllTasks = document.querySelector(".js-doneAll");
+            doneAllTasks.addEventListener("click", () => {
+                for (const task of tasks) {
+                    task.done ? task.done : task.done = !task.done
+                };
+                render();
+            });
+        };
+
+        if (tasks.length > 0) {
+            const toggleDoneTasks = document.querySelector(".js-toggleDone");
+            toggleDoneTasks.addEventListener("click", () => {
+                hideDoneTasks = !hideDoneTasks;
+                render();
+            });
+        };
+    };
+
+    const renderButtons = () => {
+        let htmlString = "";
+        if (tasks.length > 0) {
+            htmlString += `
+            <button class="js-toggleDone">${hideDoneTasks ?'Ukryj ukończone' :'Pokaż ukończone'}</button>
+            <button ${tasks.every(({ done }) => done) ? 'disabled' : ""} class="js-doneAll">Ukończ wszystkie</button>
+            `
+        }
+
+        document.querySelector(".js-buttonsAll").innerHTML = htmlString;
+    };
+
+    const renderTasks = () => {
         let htmlString = "";
 
         for (const task of tasks) {
-            htmlString += `
-                <li class="list__item">
+                htmlString += `
+                <li class="${hideDoneTasks && task.done ?'list__item--disabled' : "list__item"}">
                 <button class="list__button--done js-doneButton">${task.done ? "✔" : ""}</button>
                 <span${task.done ? " style=\"text-decoration: line-through\"" : ""} class="list__content">
                 ${task.content}</span>
                 <button class="js-removeButton list__button--remove"><i class="fa fa-trash-o"></i></button>
-           </li>
-            `;
-        }
+                </li>
+                `;
+            }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const render = () => {
+        renderTasks();
+        renderButtons();
 
         bindEvents();
+        bindAllEvent();
     };
 
     const doneTask = (index) => {
-        tasks[index].done = !tasks[index].done;
+        tasks = [
+            ...tasks.slice(0, index),
+            { ...tasks[index], done: !tasks[index].done },
+            ...tasks.slice(index + 1)
+        ];
         render();
     };
 
     const removeTask = (index) => {
-        tasks.splice(index, 1);
+        tasks = [
+            ...tasks.splice(0, index),
+            ...tasks.splice(index + 1)
+        ];
         render();
     };
 
     const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-            done: false
-        });
+        tasks = [
+            ...tasks,
+            {
+                content: newTaskContent,
+                done: false,
+            }
+        ];
 
         render();
     };
